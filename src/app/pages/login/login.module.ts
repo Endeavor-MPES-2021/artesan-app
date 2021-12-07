b773773b757b21cf7aa2219220a1665fd12ad876
Thomas Rabelo <thomas.rabelo@otimizeit.com.br>
2021-12-07 09:12:11 -0300

    - Ajuste no tamanho máximo dos textos dos cards. Issues: 22,

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes), TranslateModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, RouterModule.forChild(routes), TranslateModule],
   declarations: [LoginPage],
 })
 export class LoginPageModule {}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavController, ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { LoginService } from 'src/app/services/login/login.service';
 import { AccountService } from '../../services/auth/account.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, AfterViewInit {
   // The account fields for the login form.
-  account: { username: string; password: string; rememberMe: boolean } = {
-    username: '',
-    password: '',
-    rememberMe: false,
-  };
+  // account: { username: string; password: string; rememberMe: boolean } = {
+  //   username: '',
+  //   password: '',
+  //   rememberMe: false,
+  // };
+
+  loginForm = this.fb.group({
+    username: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+    password: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
+    rememberMe: [false],
+  });
 
   // Our translated text strings
   private loginErrorString: string;
@@ -25,7 +32,8 @@ export class LoginPage implements OnInit {
     public loginService: LoginService,
     public toastController: ToastController,
     public navController: NavController,
-    private accountService: AccountService
+    private fb: FormBuilder,
+  private accountService: AccountService
   ) {}
 
   ngOnInit() {
@@ -35,20 +43,28 @@ export class LoginPage implements OnInit {
 
     this.accountService.identity().then((account) => {
       if (account !== null) {
-        this.account = account;
+        //this.account = account;
         this.navController.navigateRoot('/tabs');
       }
     });
   }
 
+  ngAfterViewInit(): void {
+    // this.username.nativeElement.focus();
+  }
+
   doLogin() {
-    this.loginService.login(this.account).then(
+    this.loginService.login({
+      username: this.loginForm.get('username').value,
+      password: this.loginForm.get('password').value,
+      rememberMe: this.loginForm.get('rememberMe').value,
+    }).then(
       () => {
         this.navController.navigateRoot('/tabs');
       },
       async (err) => {
         // Unable to log in
-        this.account.password = '';
+        //this.loginForm.get('password').value = '';
         const toast = await this.toastController.create({
           message: this.loginErrorString,
           duration: 3000,
